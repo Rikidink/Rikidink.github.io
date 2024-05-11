203edb84ac6b5fe884b4d6e1b2b9da5532009f8e
Ricky Z <rickyzhang2013@hotmail.com>
2024-05-11 21:05:36 +1000

    test commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'Algorithmica',
+  author: 'Ricky Zhang',
+  headerTitle: 'Enclyclopedia Algorithmica',
+  description: 'A collection of algorithms and general programming stuff',
+  language: 'en-au',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://algorithmica.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
