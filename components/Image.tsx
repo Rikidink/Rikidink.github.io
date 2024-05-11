714a29ddc2e241f7b8460300e30f53e1f47d52cd
Ricky Z <rickyzhang2013@hotmail.com>
2024-05-11 22:04:05 +1000

    changed image src again

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,7 @@
 import NextImage, { ImageProps } from 'next/image'
 
 const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`algorithmica/${src}`} {...rest} />
+  <NextImage src={`/algorithmica/${src}`} {...rest} />
 )
 
 export default Image
